d3d584830947280ef891d3f91bb25627e7d965f7
Marin <marin@dev.nddesign.no>
2023-02-02 12:34:45 +0200

    better interface for the garbage collector

--- src/stubs/project/app/Console/Commands/GarbageCollector.php
+++ src/stubs/project/app/Console/Commands/GarbageCollector.php
@@ -1,70 +1,76 @@
 <?php
 
-    namespace App\Console\Commands;
+namespace App\Console\Commands;
 
-    use Illuminate\Console\Command;
-    use Carbon\Carbon;
-    use Illuminate\Pipeline\Pipeline;
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use Illuminate\Pipeline\Pipeline;
 
-    class GarbageCollector extends Command
-    {
-        /**
-         * The name and signature of the console command.
-         *
-         * @var string
-         */
-        protected $signature = 'gc:cleanup
+class GarbageCollector extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gc:cleanup
             {--t|type=* : To clean only type}';
 
-        /**
-         * The console command description.
-         *
-         * @var string
-         */
-        protected $description = 'cleanup';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'cleanup';
 
-        public static $cleaning = [];
 
-        /**
-         * Create a new command instance.
-         *
-         * @return void
-         */
-        public function __construct()
-        {
-            parent::__construct();
+    public $refComponents = null; //for the interface
 
-            static::$cleaning = [
-                'sessions' => function($command, \Closure $next) {
-                    $command->sessionGC();
-                    $command->info("Done cleaning sessions!");
-                    return $next($command);
-                },
-            ];
+    public static $cleaning = [];
 
-            // @HOOK_CLEANING
-        }
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
 
-        public function sessionGC() {
-            session()->getHandler()->gc(
-                (config('session.lifetime') ?? null) * 60 //to seconds
-            );
-        }
+        static::$cleaning = [
+            'sessions' => function($command, \Closure $next) {
+                $command->components->task("Cleaning sessions", function() use ($command){
+                    return $command->sessionGC();
 
-        /**
-         * Execute the console command.
-         *
-         * @return mixed
-         */
-        public function handle() {
-            if($types = $this->option('type')) {
-                static::$cleaning = array_intersect_key(static::$cleaning, array_flip($types));
-            }
-            app(Pipeline::class)
-                ->send($this)
-                ->through(static::$cleaning)
-                ->then(function($command){
-                     $this->info("Done!");
                 });
-        }
+                return $next($command);
+            },
+        ];
+
+        // @HOOK_CLEANING
     }
+
+    public function sessionGC() {
+        session()->getHandler()->gc(
+            (config('session.lifetime') ?? null) * 60 //to seconds
+        );
+        return true;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        if($types = $this->option('type')) {
+            static::$cleaning = array_intersect_key(static::$cleaning, array_flip($types));
+        }
+        app(Pipeline::class)
+            ->send($this)
+            ->through(static::$cleaning)
+            ->then(function($command){
+                $this->components->info("Done!");
+            });
+    }
+}
